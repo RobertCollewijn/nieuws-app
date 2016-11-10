798421f72b205149bb9e1ea8365c8b045f3878f6
Robert <robert@tooconnect.nl>
2016-11-10 07:55:12 +0100

    Nieuwsitem list
    Model
    Tonen
    Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+///<reference path="../../node_modules/rxjs/add/operator/debounceTime.d.ts"/>
+///<reference path="../../node_modules/rxjs/add/operator/distinctUntilChanged.d.ts"/>
+import {Component} from '@angular/core';
+import {Observable} from "rxjs";
+import {NieuwsItem} from "./models/nieuws-item";
+import {NieuwsItemsService} from "./services/nieuws-items.service";
+import {Response} from "@angular/http";
+
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Nieuws App !';
+
+  nieuwsItems: NieuwsItem[];
+
+  constructor(private nieuwsItemService: NieuwsItemsService) {
+    setTimeout(()=> {
+    this.nieuwsItemService.loadNieuwsItems()
+      .subscribe(
+        data => {
+          this.nieuwsItems = data
+        },
+        error => console.error(error)
+      );
+    },4);
+  }
+
+
 }
